d6a90b8ed8fb893087b893b4d7d0924acd5ea101
Vasiliy Yanchuk <yanchuk8kz@yandex.ru>
2022-07-17 15:53:59 +0600

    fix: users controller #3

--- controllers/users.js
+++ controllers/users.js
@@ -13,19 +13,16 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
   .then((user) => {
-    if(user){
-      res.status(200).send({ user });
+    if(!user){
+      return res.status(404).send({ "message": "Пользователь по указанному _id не найден." })
     }
-    else{
-      res.status(404).send({ "message": "Пользователь по указанному _id не найден." })
-    }
+    return res.status(200).send({ user });
   })
   .catch((err) => {
-    if (err.name === 'ValidationError' || err.name === 'CastError') {
-      res.status(400).send({ "message": "Введены некорректные данные" });
-    } else {
-      res.status(500).send({ "message": "Ошибка по умолчанию." });
+    if (err.name === 'CastError') {
+      return res.status(400).send({ "message": "Передан некорректный _id пользователя." });
     }
+    return res.status(500).send({ "message": "Ошибка по умолчанию." });
   })
 };
 
